8ae57c96e64187e20c671a3ff5fea1ac8cd5847e
Raulwicke <nicholas.bradford1@marist.edu>
2016-09-13 08:41:53 -0400

    Lab#3 Creation
    Created the Lab#3 folder and started the lab.

---
+++ create_limbo.sql
@@ -0,0 +1,8 @@
+#This file creates the Limbo database.
+#Author: Nicholas Bradford, Claudia Rojas, Colin May
+
+drop database if exists limbo_db ;
+create database limbo_db ;
+use limbo_db ;
+
+CREATE stuff 
