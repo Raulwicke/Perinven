73d1fe6e18dad9bd1de77810fcadc61a88f63eeb
Raulwicke <nicholas.bradford1@marist.edu>
2016-09-16 08:22:19 -0400

    Lab #3 Update
    Commented project and minor text fixes

--- Lab#3/create_limbo.sql
+++ Lab#3/create_limbo.sql
@@ -1,11 +1,18 @@
-#This file creates the Limbo database.
-#Author: Nicholas Bradford, Claudia Rojas, Colin May
-
-drop database if exists limbo_db ;
-create database limbo_db ;
-use limbo_db ;
-
-CREATE TABLE IF NOT EXISTS stuff (
-id INT UNSIGNED NOT NULL,
-desr TEXT NOT NULL,
-PRIMARY KEY (id) );
+#This file creates the Limbo database. 
+#Author: Nicholas Bradford, Claudia Rojas, Colin May 
+ 
+ 
+drop database if exists limbo_db ; 
+create database limbo_db ; 
+use limbo_db ; 
+ 
+#Creating Stuff table with an ID, a Desription, a finder-id, and a location-id with a boolean for if it has been retrieved
+CREATE TABLE IF NOT EXISTS stuff ( 
+id INT, 
+description TEXT, 
+fid int, 
+locid int,  
+retrieved BOOLEAN NOT NULL DEFAULT 0, 
+ 
+#sets the primary key to the item id
+PRIMARY KEY (id) ); 
