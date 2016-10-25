3ebc02359d2f5371d4e424176f94e5bd7b9253f4
colinomay <colin.may1@marist.edu>
2016-10-25 10:36:34 -0400

    Add files via upload

--- localweb/includes/helpers.php
+++ localweb/includes/helpers.php
@@ -1,16 +1,17 @@
-<!--Authors: Claudia Rojas, Colin May, Nicholas Bradford-->
+/*
+Authors: Claudia Rojas, Colin May, Nicholas Bradford
+*/
+
+
 <?php
 $debug = true;
-
 # Shows the records in prints
 function show_records($dbc) {
 	# Create a query to get the number, fname, lname sorted by number
 	$query = 'SELECT number, fname, lname FROM presidents ORDER BY number ASC' ;
-
 	# Execute the query
 	$results = mysqli_query( $dbc , $query ) ;
 	check_results($results) ;
-
 	# Show results
 	if( $results )
  	{
@@ -23,7 +24,6 @@ function show_records($dbc) {
    echo '<TH>First Name</TH>';
    echo '<TH>Last Name</TH>';
    echo '</TR>';
-
   # For each row result, generate a table row
    while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
    {
@@ -33,67 +33,46 @@ function show_records($dbc) {
      echo '<TD>' . $row['lname'] . '</TD>' ;
      echo '</TR>' ;
    }
-
   # End the table
    echo '</TABLE>';
-
   # Free up the results in memory
    mysqli_free_result( $results ) ;
  }
 }
-
 # Inserts a record into the prints table
 function insert_record($dbc, $number, $fname, $lname) {
   $query = 'INSERT INTO presidents(number, fname, lname) VALUES (' . $number . ' , "' . $fname . '" , "' . $lname . '")' ;
   show_query($query);
-
-   #Checks the query if the number, fnam, and lname is valid
-if (!valid_number($number))
-	echo '<p>Please give a valid number.</p>';
-else if (!valid_name($fname))
-	echo '<p>Please complete the first name.</p>';
-else if (!valid_name($lname))
-	echo '<p>Please complete the last name.</p>';
-else insert_record($dbc,$number,$fname,$lname) ; 
-  
-  
   $results = mysqli_query($dbc,$query) ;
   check_results($results) ;
-
   return $results ;
 }
-
 # Shows the query as a debugging aid
 function show_query($query) {
   global $debug;
-
   if($debug)
     echo "<p>Query = $query</p>" ;
 }
-
 # Checks the query results as a debugging aid
 function check_results($results) {
   global $dbc;
-
   if($results != true)
     echo '<p>SQL ERROR = ' . mysqli_error( $dbc ) . '</p>'  ;
 }
-
 #Checks the query if the number is valid
-function valid_number($number) {
-if(empty($number) || !is_numeric($number))
-	return false;
-else {
-	$number = intval($number) ;
-		if($number <= 0)
-			return false ;
-		}
-	return true ;
-}
-	
-function valid_name($name){
-	if (empty($name))
+function valid_number($number){
+	if(empty($number) || !is_numeric($number))
 		return false;
+	else{
+		$number = intval($number);
+		if($number <= 0)
+			return false;
+	}
 	return true;
 	}
+function valid_name($name){
+    if(empty($name))
+        return false;
+    return true;
+}
 ?>
