3c19bf6d927a28e0701785f7e8038ce0dacb401b
Nicholas D Bradford <nicholas.bradford1@marist.edu>
2016-12-01 23:57:44 -0500

    ADMIN UPDATE
    Admin Script upload to be worked on by other parties

---
+++ Perinven_Files/ADMIN UPDATE/admin.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Style-Type" content="text/css" /> 
+		<title>Limbo.com</title>
+		<link href="/library/skin/tool_base.css" type="text/css" rel="stylesheet" media="all" />
+		<link href="/library/skin/morpheus-default/tool.css" type="text/css" rel="stylesheet" media="all" />
+        <link href="limbostyles.css" type="text/css" rel="stylesheet" />
+		<script type="text/javascript" language="JavaScript" src="/library/js/headscripts.js"></script>
+
+		
+	</head>
+	<body>
+        <div class="menu">
+            <?php include 'menu.php';?>
+        </div>
+
+		<?php
+			# Create a query to get the number, fname, lname sorted by number
+			# Connect to MySQL server and the database
+            require('limbo_login_tools.php');
+           if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
+
+                $login = $_POST['login'] ;
+                $pass = $_POST['pass'] ;
+
+                $check = validate($dbc,$login, $pass) ;
+                
+                if($check == -1)
+                  echo '<P style=color:red>Login failed please try again.</P>' ;
+
+                else
+                  load('index_admin.php', $pass);
+            }
+    // show_admin_form($login, $pass) ;
+?>
+        <h1>Admin login</h1>
+<form action="admin.php" method="POST">
+<table>
+<tr>
+<td>Login:</td><td><input type="text" name="login"></td>
+</tr>
+<tr>
+<td>Password:</td><td><input type="password" name="pass"></td>
+</tr>
+</table>
+<p><input type="submit" ></p>
+
+
---
+++ Perinven_Files/ADMIN UPDATE/helpers_limbo.php
@@ -0,0 +1,236 @@
+
+
+<style><?php include "limbostyles.css"; ?></style>
+
+<?php
+$debug = true;
+function show_records($dbc){
+		$query = 'Select description, create_date, status from stuff order by create_date desc';
+		# Execute the query
+		$results = mysqli_query( $dbc , $query ) ;
+		check_results($results);
+		
+		if($results)
+		{ 
+            
+			echo "<h1>Welcome to Limbo!<br></h1>";
+			echo "<h4>If you lost or found something, you're in luck: this is the place to report it.</h4>";
+			echo "<br>";
+			echo "Reported in last [drop down menu]<br>";
+			echo '<TABLE>';
+			echo '<TR>';
+			echo '<TH>Date/Time</TH>';
+			echo '<TH>Status</TH>'; 
+			echo '<TH>Stuff</TH>';
+			echo '</TR>';
+            echo "</br>";
+            # For each row result, generate a table row
+			while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+			{
+				echo '<TR>' ;
+				echo '<TD>' . $row['create_date'] . '</TD>' ;
+				echo '<TD>' . $row['status'] . '</TD>' ;
+				echo '<TD>' . $row['description'] . '</TD>' ;
+				echo '</TR>' ;
+			}
+			# End the table
+			echo '</TABLE>';
+			mysqli_free_result( $results);
+		}
+		else{
+			echo "Something has gone wrong";
+		
+		}
+	}
+function show_found($dbc){
+		$query = 'SELECT stuff.description, stuff.create_date, stuff.finder, locations.name
+                  FROM stuff, locations 
+                  WHERE status = "found" AND
+                  locations.location_id = stuff.location_id
+                  ORDER BY stuff.create_date desc';
+		# Execute the query
+		$results = mysqli_query( $dbc , $query ) ;
+		check_results($results);
+		
+		if($results)
+		{ 
+            
+			echo "<h1>Welcome to Limbo!<br></h1>";
+			echo "<h4>You've found something! Thanks for reporting it!</h4>";
+			echo '<TABLE>';
+			echo '<TR>';
+			echo '<TH>Stuff</TH>';
+			echo '<TH>Finder</TH>'; 
+			echo '<TH>Date</TH>';
+            echo '<TH>Location</TH>';
+			echo '</TR>';
+            echo "</br>";
+            # For each row result, generate a table row
+			while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+			{
+				echo '<TR>' ;
+				echo '<TD>' . $row['description'] . '</TD>' ;
+				echo '<TD>' . $row['finder'] . '</TD>' ;
+				echo '<TD>' . $row['create_date'] . '</TD>' ;
+                echo '<TD>' . $row['name'] . '</TD>' ;
+				echo '</TR>' ;
+			}
+			# End the table
+			echo '</TABLE>';
+			mysqli_free_result( $results);
+		}
+		else{
+			echo "Something has gone wrong";
+		
+		}
+	}
+function show_lost($dbc){
+		$query = 'SELECT s.description, s.create_date, s.owner, l.name
+                  FROM stuff as s, locations as l 
+                  WHERE status = "lost" AND
+                  l.location_id = s.location_id
+                  ORDER BY s.create_date desc';
+		# Execute the query
+		$results = mysqli_query( $dbc , $query ) ;
+		check_results($results);
+		
+		if($results)
+		{ 
+            
+			echo "<h1>Welcome to Limbo!<br></h1>";
+			echo "<h4>You've Lost something! Let's see if we can help you find it!!</h4>";
+			echo '<TABLE>';
+			echo '<TR>';
+			echo '<TH>Stuff</TH>';
+			echo '<TH>Finder</TH>'; 
+			echo '<TH>Date</TH>';
+            echo '<TH>Location</TH>';
+			echo '</TR>';
+            echo "</br>";
+            # For each row result, generate a table row
+			while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ))
+			{
+				echo '<TR>' ;
+				echo '<TD>' . $row['description'] . '</TD>' ;
+				echo '<TD>' . $row['owner'] . '</TD>' ;
+				echo '<TD>' . $row['create_date'] . '</TD>' ;
+                echo '<TD>' . $row['name'] . '</TD>' ;
+				echo '</TR>' ;
+			}
+			# End the table
+			echo '</TABLE>';
+			mysqli_free_result( $results);
+		}
+		else{
+			echo "Something has gone wrong";
+		
+		}
+	}
+function check_results($results){
+	global $dbc;
+	$arrayName = array('Select Location', );
+
+	if (!$results){
+		echo "<p> SQL ERROR = ". mysqli_error( $dbc ) . '</p>';
+	}
+}
+function show_lost_form($description,$owner,$locations, $room) {
+	global $dbc;
+  echo '<form action="addLost.php" method="POST">' ;
+
+  echo '<p>Description/Name: <input type="text" name="description" value="' . $description . '"> </p> ' ;
+
+
+  echo '<p>Owner Name: <input type="text" name="owner" value="' . $owner . '"></p>' ;
+
+  $sql="SELECT name, location_id FROM locations ORDER BY name";
+  echo '<select name="locations" value="">Location</option>';
+	foreach ($dbc->query($sql) as $row){
+		echo "<option value=$row[location_id]>$row[name]</option>";
+	}
+  echo "</select>"; 
+  
+  echo '<p>Room: <input type="text" name="room" value="' . $room . '"></p>' ;
+
+  echo '<p><input type="submit"></p>' ;
+  echo '</form>' ;
+}
+function insert_lost_record($dbc, $description, $owner, $location, $room) {
+  $status= 'lost';
+  $query = 'INSERT INTO stuff(location_id,description,create_date,update_date,room,owner,finder,status) 
+  			VALUES ("' . $location . '","' . $description . '" ,now(),now(), "' . $room . '","' . $owner . ' ",Null, "'. $status . '")' ;
+
+  $results = mysqli_query($dbc,$query) ;
+  check_results($results) ;
+  return $results ;
+}
+function insert_found_record($dbc, $description, $finder, $location, $room) {
+  $status= 'found';
+  $query = 'INSERT INTO stuff(location_id,description,create_date,update_date,room,owner,finder,status) 
+  			VALUES ("' . $location . '","' . $description . '" ,now(),now(), "' . $room . '",Null,"' . $finder . ' ","'. $status . '")' ;
+
+  $results = mysqli_query($dbc,$query) ;
+  check_results($results) ;
+  return $results ;
+}
+function show_found_form($description,$finder,$locations, $room) {
+	global $dbc;
+  echo '<form action="addFound.php" method="POST">' ;
+
+  echo '<p>Description/Name: <input type="text" name="description" value="' . $description . '"> </p> ' ;
+
+  echo '<p>Finder Name: <input type="text" name="finder" value="' . $finder . '"></p>' ;
+
+  $sql="SELECT name, location_id FROM locations ORDER BY name";
+  echo '<select name="locations" value="">Location</option>';
+	foreach ($dbc->query($sql) as $row){
+		echo "<option value=$row[location_id]>$row[name]</option>";
+	}
+  echo "</select>"; 
+  
+  echo '<p>Room: <input type="text" name="room" value="' . $room . '"></p>' ;
+
+  echo '<p><input type="submit"></p>' ;
+  echo '</form>' ;
+}
+function show_admin_records($dbc){
+		$query = 'Select description, create_date, status from stuff order by create_date desc';
+		# Execute the query
+		$results = mysqli_query( $dbc , $query ) ;
+		check_results($results);
+		
+		if($results)
+		{ 
+            
+			echo "<h1>Welcome to Limbo!<br></h1>";
+			echo "<h4>If you lost or found something, you're in luck: this is the place to report it.</h4>";
+			echo "<br>";
+			echo "Reported in last [drop down menu]<br>";
+			echo '<TABLE>';
+			echo '<TR>';
+			echo '<TH>Date/Time</TH>';
+			echo '<TH>Status</TH>'; 
+			echo '<TH>Stuff</TH>';
+			echo '<TH> </TH>';
+			echo '</TR>';
+            echo "</br>";
+            # For each row result, generate a table row
+			while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+			{
+				echo '<TR>' ;
+				echo '<TD>' . $row['create_date'] . '</TD>' ;
+				echo '<TD>' . $row['status'] . '</TD>' ;
+				echo '<TD>' . $row['description'] . '</TD>' ;
+				echo '<TD><form action='' method='post'><button name='submit'>Edit</button></form>';
+				echo '</TR>' ;
+			}
+			# End the table
+			echo '</TABLE>';
+			mysqli_free_result( $results);
+		}
+		else{
+			echo "Something has gone wrong";
+		
+		}
+	}
+?>
