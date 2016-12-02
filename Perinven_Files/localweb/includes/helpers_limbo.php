4ede89c987f614c9208db2d7a3c485638926e8af
ClaudLucia <claudialurojas@gmail.com>
2016-12-01 23:00:07 -0500

    Add files via upload

--- Perinven_Files/localweb/includes/helpers_limbo.php
+++ Perinven_Files/localweb/includes/helpers_limbo.php
@@ -4,7 +4,7 @@
 
 <?php
 $debug = true;
-function show_records($dbc){
+function show_records($dbc, ){
 		$query = 'Select description, create_date, status from stuff order by create_date desc';
 		# Execute the query
 		$results = mysqli_query( $dbc , $query ) ;
@@ -13,17 +13,6 @@ function show_records($dbc){
 		if($results)
 		{ 
             
-			echo "<h1>Welcome to Limbo!<br></h1>";
-			echo "<h4>If you lost or found something, you're in luck: this is the place to report it.</h4>";
-			echo "<br>";
-			echo "Reported in last [drop down menu]<br>";
-			echo '<TABLE>';
-			echo '<TR>';
-			echo '<TH>Date/Time</TH>';
-			echo '<TH>Status</TH>'; 
-			echo '<TH>Stuff</TH>';
-			echo '</TR>';
-            echo "</br>";
             # For each row result, generate a table row
 			while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
 			{
@@ -43,7 +32,7 @@ function show_records($dbc){
 		}
 	}
 function show_found($dbc){
-		$query = 'SELECT stuff.description, stuff.create_date, stuff.finder, locations.name
+		$query = 'SELECT description, stuff.create_date, finder, locations.name
                   FROM stuff, locations 
                   WHERE status = "found" AND
                   locations.location_id = stuff.location_id
@@ -138,21 +127,19 @@ function show_lost_form($description,$owner,$locations, $room)
 {
 	global $dbc;
   echo '<form action="addLost.php" method="POST">' ;
-
   echo '<p>Description/Name: <input type="text" name="description" value="' . $description . '"> </p> ' ;
 
-
   echo '<p>Owner Name: <input type="text" name="owner" value="' . $owner . '"></p>' ;
-
   $sql="SELECT name, location_id FROM locations ORDER BY name";
+
   echo '<select name="locations" value="">Location</option>';
+
 	foreach ($dbc->query($sql) as $row){
 		echo "<option value=$row[location_id]>$row[name]</option>";
 	}
   echo "</select>"; 
   
   echo '<p>Room: <input type="text" name="room" value="' . $room . '"></p>' ;
-
   echo '<p><input type="submit"></p>' ;
   echo '</form>' ;
 }
@@ -160,30 +147,22 @@ function show_lost_form($description,$owner,$locations, $room)
 
 function insert_lost_record($dbc, $description, $owner, $location, $room) {
   $status= 'lost';
+  
   $query = 'INSERT INTO stuff(location_id,description,create_date,update_date,room,owner,finder,status) 
-  			VALUES ("' . $location . '","' . $description . '" ,now(),now(), "' . $room . '","' . $owner . ' ",Null, "'. $status . '")' ;
-
+  			VALUES ("' . $results . '","' . $description . '" ,now(),now(), "' . $room . '","' . $owner . ' ",Null, "'. $status . '")' ;
   $results = mysqli_query($dbc,$query) ;
   check_results($results) ;
   return $results ;
 }
-function insert_found_record($dbc, $description, $finder, $location, $room) {
-  $status= 'found';
-  $query = 'INSERT INTO stuff(location_id,description,create_date,update_date,room,owner,finder,status) 
-  			VALUES ("' . $location . '","' . $description . '" ,now(),now(), "' . $room . '",Null,"' . $finder . ' ","'. $status . '")' ;
 
-  $results = mysqli_query($dbc,$query) ;
-  check_results($results) ;
-  return $results ;
-}
-function show_found_form($description,$finder,$locations, $room) 
+function show_find_form($description,$finder,$locations, $room) 
 {
 	global $dbc;
   echo '<form action="addFound.php" method="POST">' ;
 
   echo '<p>Description/Name: <input type="text" name="description" value="' . $description . '"> </p> ' ;
 
-  echo '<p>Finder Name: <input type="text" name="finder" value="' . $finder . '"></p>' ;
+  echo '<p>Finder Name: <input type="text" name="name" value="' . $finder . '"></p>' ;
 
   $sql="SELECT name, location_id FROM locations ORDER BY name";
   echo '<select name="locations" value="">Location</option>';
@@ -197,5 +176,16 @@ function show_found_form($description,$finder,$locations, $room)
   echo '<p><input type="submit"></p>' ;
   echo '</form>' ;
 }
+function show_admin_form($login, $pass) 
+{
+    global $dbc;
+  echo '<form action="admin.php" method="POST">' ;
 
+  echo '<p>Login: <input type="text" name="login" value="' . $login . '"> </p> ' ;
+
+  echo '<p>Password: <input type="text" name="pass" value="' . $pass . '"></p>' ;
+
+  echo '<p><input type="submit"></p>' ;
+  echo '</form>' ;
+}
 ?>
